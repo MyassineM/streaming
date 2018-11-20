ec0d8f725694d5444b12c699170a441ba0072212
streaming-yassine <44115214+MyassineM@users.noreply.github.com>
2018-11-20 11:18:46 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const discord_token = "NDI4NTgyNTYwNDQwNzc4NzU3.DtLaxA.FfAcWG28LPQO26vLr7LK64nFoA4";
-const prefix = "!"
-client.login(process.env.BOT_TOKEN);
+const prefix = "$"
+client.login(process.env.BOT_TOKEN)
 client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
@@ -9,21 +9,21 @@ client.on('message', message => {
 
 if (message.content.startsWith(prefix + 'g')) {
   client.user.setGame(argresult);
-    message.channel.sendMessage(`**✅  : !{argresult}**`)
+    message.channel.sendMessage(`**✅  : ${argresult}**`)
 } else 
 
 if (message.content.startsWith(prefix + 'w')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(`**✅  : !{argresult}**`)
+    message.channel.sendMessage(`**✅  : ${argresult}**`)
 } else 
 if (message.content.startsWith(prefix + 'l')) {
 client.user.setActivity(argresult, {type:'LISTENING'});
-    message.channel.sendMessage(`**✅  : !{argresult}**`)
+    message.channel.sendMessage(`**✅  : ${argresult}**`)
 } else 
 
 if (message.content.startsWith(prefix + 's')) {
   client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
-    message.channel.sendMessage(`**✅  : !{argresult}**`)
+    message.channel.sendMessage(`**✅  : ${argresult}**`)
 }
 
 });
